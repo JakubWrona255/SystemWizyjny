551cefbc6cd7bfc6ad0aea31050cdee05fc07bdd
marsok98 <49905713+marsok98@users.noreply.github.com>
2022-11-05 14:56:18 +0100

    missed main during commit

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ import ImageProcess as imgp
 import cv2 as cv
 #__all__ = []
 
-filename = 'img/bent_1.jpg'
 
-image = imgp.readImg(filename)
+filenameOrigin = 'img/pointCut_'
+for i in range(1,5,1):
+    filename = filenameOrigin + str(i) + '.jpg'
+    print(filename)
+    print("----")
+    image = imgp.readImg(filename)
 
-yMin, yMax, xMin, xMax = imgp.findNail(image, scale_factor=0.15, gammaFactor=1,threshold=70)
+    yMin, yMax, xMin, xMax = imgp.findNail(image, scale_factor=0.15, gammaFactor=1, threshold=70)
 
-imageCropped = imgp.cropImage(image, yMin, yMax, xMin, xMax, extraHeight=0.05, extraWidth=0.05)
+    imageCropped = imgp.cropImage(image, yMin, yMax, xMin, xMax, extraHeight=0.05, extraWidth=0.05)
 
-error = imgp.lookForDefects(imageCropped,gammaCorFactor=1, threshold=50)
+    error = imgp.lookForDefects(imageCropped, gammaCorFactor=1, threshold=50)
+
+
+
+
+
 
 
